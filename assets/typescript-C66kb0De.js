ddd0c1ebd98ed37a8ef18a1be980092d0a9d159c
wesmile2020 <1211921908@qq.com>
2025-12-04 08:58:50 +0000

    Deploying to pages from @ wesmile2020/convert_json2dts@b4ecd398b3ee8d610ca06913911b79956bb081bc 🚀

---
+++ assets/typescript-C66kb0De.js
@@ -0,0 +1,1 @@
+import{n as e,t}from"./typescript-DkL54wrH.js";export{t as conf,e as language};
---
+++ index.d.ts
@@ -0,0 +1,8 @@
+export declare class JSON2Dts {
+    transformByJSON(data: unknown): string;
+    transformByJSONString(code: string): string;
+    convertJSONToDts(data: unknown): string;
+    convertJSONStringToDts(code: string): string;
+}
+
+export declare const version: string;
